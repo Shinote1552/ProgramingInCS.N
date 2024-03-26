7530d6d4832f330953ed9ff6f00ecaa4c1f22e3a
Your Name <musatatarmalae@gmail.com>
2024-03-26 04:04:58 +0300

    hello2

--- ConsoleApp1HELLO/Program.cs
+++ ConsoleApp1HELLO/Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.Write("Введите свое имя: ");
 var name = Console.ReadLine();       // вводим имя
 Console.WriteLine($"Привет {name}");    // выводим имя на консоль
+
+// asdasdsada
+//65465465
